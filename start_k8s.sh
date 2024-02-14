17501b8d55edd219c285b747b5c9d6f947f4e8ea
dreampath88 <dreampath88@gmail.com>
2024-02-14 15:50:54 +0000

    update yaml

--- start_k8s.sh
+++ start_k8s.sh
@@ -32,8 +32,27 @@ if ! kubectl get namespace final > /dev/null 2>&1; then
     kubectl create ns final && \
 fi
 
-# Apply k8s yaml files
-kubectl apply -f ./manifests && \
+# Apply k8s service and ingress yaml files
+kubectl apply -f ./manifests/service.yaml && \
+kubectl apply -f ./manifests/ingress.yaml && \
+
+# Update alb url
+url=$(kubectl get ing django-alb -n final | tail -n 1 | awk -F' ' '{printf $4}')
+while [ -z "${url}" ]; do
+    echo Not found alb url
+    kubectl describe ing -n final django-alb
+    sleep 1
+done
+echo Update the alb ulr: $url
+
+sed -E -i.bak1 "s|DJANGO_BASE_URL: http://[0-9a-zA-Z\.-]+|DJANGO_BASE_URL: http://${url}|g" ./manifests/configmap.yaml && \
+sed -E -i.bak2 "s|DJANGO_ALLOWED_HOSTS: https://d11k7zd8ekz887.cloudfront.net http://[0-9a-zA-Z\.-]+|DJANGO_ALLOWED_HOSTS: https://d11k7zd8ekz887.cloudfront.net http://${url}|g" ./manifests/configmap.yaml && \
+
+mv -v manifests/*.yaml.bak* tmp/ && \
+
+# Apply configmap and deployment yaml files
+kubectl apply -f ./manifests/configmap.yaml && \
+kubectl apply -f ./manifests/deployment.yaml && \
 
 # Install metric servers
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml && \
